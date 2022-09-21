d0e7389811adde88f99cad761992b6f4005369a0
Anirudh Dontha <Anirudh.Dontha@jbhunt.com>
2022-09-21 15:16:01 -0500

    Main page: Media Queries and Images

--- src/app/carousel/carousel.component.ts
+++ src/app/carousel/carousel.component.ts
@@ -8,19 +8,38 @@ import { Component, OnInit } from '@angular/core';
 export class CarouselComponent implements OnInit {
 
   imagesPath!: string[];
+  responsiveOptions: any;
 
-  constructor() { }
+  constructor() {
+    this.responsiveOptions = [
+      {
+        breakpoint: '1024px',
+        numVisible: 3,
+        numScroll: 3
+      },
+      {
+        breakpoint: '768px',
+        numVisible: 2,
+        numScroll: 2
+      },
+      {
+        breakpoint: '560px',
+        numVisible: 1,
+        numScroll: 1
+      }
+    ];
+  }
 
   ngOnInit(): void {
 
     this.imagesPath = [
-      'assets/pictures/image-1.jpeg',
-      'assets/pictures/image-3.jpeg',
-      'assets/pictures/image-4.jpeg',
-      'assets/pictures/image-5.jpeg',
-      'assets/pictures/image-6.jpeg',
-      'assets/pictures/image-7.jpeg',
-      'assets/pictures/image-8.jpeg'];
+      'assets/pictures/mainPage/logo_f1.jpg',
+      'assets/pictures/mainPage/logo_f2.jpeg',
+      'assets/pictures/mainPage/logo_f3.jpeg',
+      'assets/pictures/mainPage/logo_fe.png',
+      'assets/pictures/mainPage/logo_mgp.jpeg',
+      
+    ];
   }
 
 }
