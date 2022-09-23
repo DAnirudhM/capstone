ec9b1a8d675ca726a5f2a67f68fb2c31addafca5
Anirudh <anirudhdams@gmail.com>
2022-09-23 01:08:42 -0500

    Fixed menu bar

--- src/app/components/tab-menu/tab-menu.component.ts
+++ src/app/components/tab-menu/tab-menu.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, ActivatedRouteSnapshot, NavigationStart, Router } from '@angular/router';
+import { Router } from '@angular/router';
 import { MenuItem } from 'primeng/api/menuitem';
-import { map, Observable, of } from 'rxjs';
 import { Groups } from 'src/app/models/groups.model';
 import { Organizations } from 'src/app/models/organizations.model';
 import { GroupsService } from 'src/app/services/groups.service';
@@ -14,13 +13,11 @@ import { GroupsService } from 'src/app/services/groups.service';
 export class TabMenuComponent implements OnInit {
   groups: MenuItem[] = [];
   groupsObject!: Groups[];
-  selectedOrganization!: Organizations;
 
   constructor(private router: Router, private groupsService: GroupsService) {
 
-
-    this.selectedOrganization = history.state.currentOrganization;
-    this.groupsService.getGroupsByOrganization(this.selectedOrganization.OrganizationId)
+    const orgId = this.lookUpOrgID(this.router.url);
+    this.groupsService.getGroupsByOrganization(orgId)
       .subscribe({
         next: (value: Groups[]) => {
           this.initializeGroups(value);
@@ -28,32 +25,47 @@ export class TabMenuComponent implements OnInit {
         error: (error: any) => handleError(error),
         complete: () => void (0)
       });
-
-
-
-
   }
 
   initializeGroups(groups: Groups[]): void {
-    groups.forEach(group => this.groups.push({label: group.GroupName}));
+    groups.forEach(group => this.groups.push({ label: group.GroupName }));
   }
 
   ngOnInit(): void {
     console.log(this.router.url);
-    
- 
-    // this.groups = [
-    //   { label: 'RedBull' },
-    //   { label: 'Ferrari' },
-    //   { label: 'Mercedes' },
-    //   { label: 'Alpine' },
-    //   { label: 'McLaren' },
-    //   { label: 'Alpha Romeo' },
-    //   { label: 'Haas' },
-    //   { label: 'Alpha Tauri' },
-    //   { label: 'Aston Martin' },
-    //   { label: 'Williams' }
-    // ];
+
+    this.groups = [
+      { label: 'RedBull' },
+      { label: 'Ferrari' },
+      { label: 'Mercedes' },
+      { label: 'Alpine' },
+      { label: 'McLaren' },
+      { label: 'Alpha Romeo' },
+      { label: 'Haas' },
+      { label: 'Alpha Tauri' },
+      { label: 'Aston Martin' },
+      { label: 'Williams' },
+      { label: 'Alpha Tauri' },
+      { label: 'Aston Martin' },
+      { label: 'Williams' }
+    ];
+  }
+
+  lookUpOrgID(path: string): string {
+    switch (path) {
+      case '/f-1':
+        return '1';
+      case '/f-2':
+        return '2';
+      case '/f-3':
+        return '3';
+      case '/f-e':
+        return '4';
+      case '/motogp':
+        return '5';
+      default:
+        return '/';
+    }
   }
 
 }
